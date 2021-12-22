2312bf26f7b1bbec558c3ce93d1a1a0012849955
Sheila Kerber <sheila.skerber@gmail.com>
2021-12-22 16:34:00 -0300

    creates main html and style for NewRegisterProductModal

--- src/components/NewRegisterProductModal/index.tsx
+++ src/components/NewRegisterProductModal/index.tsx
@@ -1,4 +1,7 @@
 import Modal from 'react-modal'
+import { Container } from './styles'
+import saveImg from '../../assets/save.jpg'
+import cancelImg from '../../assets/cancel.jpg'
 
 interface newRegisterNewProductModalProps {
     isOpen: boolean;
@@ -9,9 +12,27 @@ export function NewRegisterProductModal( {isOpen, onRequestClose}: newRegisterNe
     return (
         <Modal
         isOpen={isOpen}
-        onRequestClose={onRequestClose}>
-                
-        <h2>meu modal!</h2>
-      </Modal>
+        onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+        >      
+            <Container>
+                <h2>Cadastro de produto no sistema</h2>
+                <input type="text" placeholder='Novo produto'/>
+                <div className='buttonsDiv'>
+                    <button type="submit" id="saveBtn" >
+                        <img src={saveImg} alt="" />
+                        Salvar
+                    </button>
+
+                    <button type="submit" id="cancelBtn">
+                        <img src={cancelImg} alt="" />
+                        Cancelar
+                    </button>
+                </div>   
+            </Container>  
+        
+        
+        </Modal>
     )
 }
--- src/components/NewRegisterProductModal/styles.ts
+++ src/components/NewRegisterProductModal/styles.ts
@@ -1,5 +1,58 @@
 import styled  from "styled-components";
 
-export const Container = `
+export const Container = styled.form`
+    .buttonsDiv {
+        display: flex;
+    }
+    
+    h2 {
+        color: var(--darkPurple);
+        font-size: 1.5rem;
+        margin-bottom: 2rem;
+        display: block;
+        text-align: center;
+    }
 
+    input {
+            width: 100%;
+            padding: 1.5rem;
+            height: 4rem;
+            border-radius: 0.25rem;
+            border: 1px solid #d7d7d7;
+            background: #f9f1fe;
+            font-size: 1rem;
+            margin-bottom: 3rem;
+        }
+
+        button[type="submit"] { 
+            width: 50%;
+            background: var(--background);
+            border-radius: 2rem;
+            border: 1px solid #7f7f7f;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0 1rem;
+            padding: 0.3rem;
+            transition: hover 0.2s;
+            font-weight: 500;
+            font-size: 1rem;
+            
+            img {
+                width: 15%;
+                margin-right: 1rem;
+            }
+        }
+
+        #saveBtn:hover {
+                border: 1px solid #028A0F;
+                color: #028A0F;
+                font-weight: 700;
+            }
+
+            #cancelBtn:hover {
+                border: 1px solid #b22222;
+                color: #b22222;
+                font-weight: 700;
+            }
 `
