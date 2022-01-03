50a631a60147d3b9b3ea71968f14cad4ebc5bfd2
Sheila Kerber <sheila.skerber@gmail.com>
2022-01-03 20:55:33 -0300

    adds missing info about products to the seeds server

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App'
-import { createServer, Model } from 'miragejs'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import { createServer, Model } from "miragejs";
 
 createServer({
-
   models: {
     product: Model,
   },
@@ -14,40 +13,39 @@ createServer({
       products: [
         {
           id: 1,
-          productName: 'produto teste 1'
+          productName: "produto teste 1",
+          incomeQuantity: 50,
+          outcomeQuantity: 10,
+          balanceQuantity: 40,
         },
         {
           id: 2,
-          productName: 'produto teste 2'
+          productName: "produto teste 2",
+          incomeQuantity: 30,
+          outcomeQuantity: 10,
+          balanceQuantity: 20,
         },
-        {
-          id: 3,
-          productName: 'produto teste 3'
-        }
-      ]
-    })
+      ],
+    });
   },
 
   routes() {
-    this.namespace = 'api'
+    this.namespace = "api";
 
-    this.get('/products', () => {
-      return this.schema.all('product')
-    })
+    this.get("/products", () => {
+      return this.schema.all("product");
+    });
 
-    this.post('/products', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
-      return schema.create('product', data)
-    })
-  }
-})
-
-
-
+    this.post("/products", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("product", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
