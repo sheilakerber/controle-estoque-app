463206b661ef69d4d18b972000612ba5bd6baf7d
Sheila Kerber <sheila.skerber@gmail.com>
2022-01-04 20:38:29 -0300

    gets the product name, income, outcome and balance from the selectedProduct dropdown and renders it into NewEditProductModal

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,7 @@ export function App() {
       <NewEditProductModal
         isOpen={isNewEditProductModalOpen}
         onRequestClose={() => setIsNewEditProductModalOpen(false)}
+        selectedProduct={selectedProduct}
       />
       <GlobalStyle />
     </ProductsProvider>
--- src/components/NewEditProductModal/index.tsx
+++ src/components/NewEditProductModal/index.tsx
@@ -1,70 +1,107 @@
-import Modal from 'react-modal'
-import { Container } from './styles'
-import saveImg from '../../assets/save.jpg'
-import cancelImg from '../../assets/cancel.jpg'
-import deleteImg from '../../assets/delete.png'
-import closeImg from "../../assets/close.svg"
+import Modal from "react-modal";
+import { Container } from "./styles";
+import saveImg from "../../assets/save.jpg";
+import cancelImg from "../../assets/cancel.jpg";
+import deleteImg from "../../assets/delete.png";
+import closeImg from "../../assets/close.svg";
+import { useContext } from "react";
+import { ProductsContext } from "../../ProductsContext";
+import { Product } from "../../interfaces/Product";
+import { useEffect, useState } from "react";
 
-interface newEditProductModalProps {
-    isOpen: boolean;
-    onRequestClose: () => void;
+interface NewEditProductModalProps {
+  isOpen: boolean;
+  selectedProduct: Product | undefined;
+  onRequestClose: () => void;
 }
 
-export function NewEditProductModal( {isOpen, onRequestClose}: newEditProductModalProps ) {
-    return (
-       <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            overlayClassName="react-modal-overlay"
-            className="react-modal-content"
-       >  
+export function NewEditProductModal({
+  isOpen,
+  onRequestClose,
+  selectedProduct,
+}: NewEditProductModalProps) {
+  const context = useContext(ProductsContext);
 
-       <button
-            type="button"
-            onClick={onRequestClose}
-            className='react-modal-close'>
-            <img src={closeImg} alt="Fechar modal" />
-       </button>
+  let productName = "Produto nao selecionado";
+  let income = 200;
+  let outcome = 0;
+  
+  if (selectedProduct) {
+    productName = selectedProduct.productName;
+    income = selectedProduct.incomeQuantity;
+    outcome = selectedProduct.outcomeQuantity;
+  }
 
-            <Container>
-                <header>
-                    <h2>Editar produto</h2>
-                    <input type="text" placeholder='produto selecionado' disabled/>
-                </header>
-                
-                
-                <main>
-                    <div className="productEdition productInfo">
-                        <h3>Entrada</h3>
-                        <input type="text" name="" id="" /> <br />
+  // estados temporarios para as variaveis em edicao
+  const [incomeEdition, setIncomeEdition] = useState(income);
+  const [outcomeEdition, setOutcomeEdition] = useState(outcome);
 
-                        <h3>Saída</h3>
-                        <input type="text" name="" id="" /> <br />
+  useEffect(() => {
+    setIncomeEdition(income);
+    setOutcomeEdition(outcome);
+  }, [income, outcome]);
 
-                        <h3>Total em estoque</h3>
-                        <input type="text" name="" id="" disabled/> <br />
-                    </div>
+  const balance = incomeEdition - outcomeEdition
 
-                    <div className="productEdition buttonsEdition">
-                        <button type="submit" id="saveEditionButton">
-                            <img src={saveImg} alt="" />
-                            Salvar
-                        </button>
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
 
-                        <button type="submit" id="cancelEditionButton">
-                            <img src={cancelImg} alt="" />
-                            Cancelar
-                        </button>
+      <Container>
+        <header>
+          <h2>Editar produto</h2>
+          <input type="text" placeholder={productName} disabled />
+        </header>
 
-                        <button type="submit" id="deleteEditionButton">
-                            <img src={deleteImg} alt="" />
-                            Excluir
-                        </button>
-                    </div>
+        <main>
+          <div className="productEdition productInfo">
+            <h3>Entrada</h3>
+            <input
+              type="number"
+              value={incomeEdition}
+              onChange={(e) => setIncomeEdition(Number(e.target.value))}
+            />{" "}
+            <br />
+            <h3>Saída</h3>
+            <input
+              type="number"
+              value={outcomeEdition}
+              onChange={(e) => setOutcomeEdition(Number(e.target.value))}
+            />{" "}
+            <br />
+            <h3>Total em estoque</h3>
+            <input value={balance}  disabled /> <br />
+          </div>
 
-                </main>
-            </Container>
-            
-       </Modal>
-    )
+          <div className="productEdition buttonsEdition">
+            <button type="submit" id="saveEditionButton">
+              <img src={saveImg} alt="" />
+              Salvar
+            </button>
+
+            <button type="submit" id="cancelEditionButton">
+              <img src={cancelImg} alt="" />
+              Cancelar
+            </button>
+
+            <button type="submit" id="deleteEditionButton">
+              <img src={deleteImg} alt="" />
+              Excluir
+            </button>
+          </div>
+        </main>
+      </Container>
+    </Modal>
+  );
 }
